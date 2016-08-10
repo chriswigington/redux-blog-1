b54fe91ae4fbeb0b91bd86311e06d3d4c933eb6c
chriswigington <chriswigington@gmail.com>
2016-08-10 13:29:30 -0400

    Fixed AuthorsDropdown className

--- src/Components/AuthorsDropdown.js
+++ src/Components/AuthorsDropdown.js
@@ -8,7 +8,7 @@ class AuthorsDropdown extends Component {
 
   render() {
     return (
-      <select className="AuthorsList" onSelect={this.handleSelect}>
+      <select className="AuthorsDropdown" onSelect={this.handleSelect}>
         <option value="choose an author">choose an author</option>
         {this.props.authorsList.map(function(author) {
           return <Author value={author.id} key={author.id} name={author.name} />
